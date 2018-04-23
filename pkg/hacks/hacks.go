bb11fd8d392befb6dc696377b16d29759ca68a9e
Jennifer Blight <jgblight@gmail.com>
2018-04-22 22:01:09 -0700

    goimports

--- pkg/hacks/hacks.go
+++ pkg/hacks/hacks.go
@@ -1,95 +1,96 @@
 package hacks
 
 import (
-  "time"
-  "github.com/jgblight/matasano/pkg/utils"
-  "github.com/jgblight/matasano/pkg/rng"
+	"time"
+
+	"github.com/jgblight/matasano/pkg/rng"
+	"github.com/jgblight/matasano/pkg/utils"
 )
 
 const (
-  a uint32 = 2567483615
-  b uint32 = 2636928640
-  c uint32 = 4022730752
-  u_mask uint32 = 2147483648
-  l_mask uint32 = 2147483647
-  u = 11
-  s = 7
-  t = 15
-  l = 18
+	a      uint32 = 2567483615
+	b      uint32 = 2636928640
+	c      uint32 = 4022730752
+	u_mask uint32 = 2147483648
+	l_mask uint32 = 2147483647
+	u             = 11
+	s             = 7
+	t             = 15
+	l             = 18
 )
 
 func IsECB(ciphertext []byte, blockSize int) bool {
-  for i := 0; i < len(ciphertext); i+=blockSize {
-    chunkOne := ciphertext[i:i+blockSize]
-    for j := 0; j < len(ciphertext); j+= blockSize {
-      chunkTwo := ciphertext[j:j+blockSize]
-      if i != j && utils.HammingDistance(chunkOne, chunkTwo) == 0 {
-        return true
-      }
-    }
-  }
-  return false
+	for i := 0; i < len(ciphertext); i += blockSize {
+		chunkOne := ciphertext[i : i+blockSize]
+		for j := 0; j < len(ciphertext); j += blockSize {
+			chunkTwo := ciphertext[j : j+blockSize]
+			if i != j && utils.HammingDistance(chunkOne, chunkTwo) == 0 {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func UndoRightShift(y, shift uint32) uint32 {
-  x := (y & utils.CreateMask(0, shift))
-  for i := shift; i < 32; i += shift {
-    xShifted := x >> shift
-    mask := utils.CreateMask(i, i+shift)
-    x = (x & utils.CreateMask(0, i)) | ((y ^ xShifted) & mask)
-  }
-  return x
+	x := (y & utils.CreateMask(0, shift))
+	for i := shift; i < 32; i += shift {
+		xShifted := x >> shift
+		mask := utils.CreateMask(i, i+shift)
+		x = (x & utils.CreateMask(0, i)) | ((y ^ xShifted) & mask)
+	}
+	return x
 }
 
 func UndoLeftShift(y, shift, con uint32) uint32 {
-  x := (y & utils.CreateMask(32-shift, 32))
-  for i := 32 - shift; i >= 0 && i < 32; i -= shift {
-    xShifted := (x << shift) & con
-    maskStart := uint32(0)
-    if shift < i {
-        maskStart = i - shift
-    }
-    mask := utils.CreateMask(maskStart, i)
-    x = (x & utils.CreateMask(i, 32)) | ((y ^ xShifted) & mask)
-  }
-  return x
+	x := (y & utils.CreateMask(32-shift, 32))
+	for i := 32 - shift; i >= 0 && i < 32; i -= shift {
+		xShifted := (x << shift) & con
+		maskStart := uint32(0)
+		if shift < i {
+			maskStart = i - shift
+		}
+		mask := utils.CreateMask(maskStart, i)
+		x = (x & utils.CreateMask(i, 32)) | ((y ^ xShifted) & mask)
+	}
+	return x
 }
 
 func Untemper(z uint32) uint32 {
-  y := UndoRightShift(z, l)
-  y = UndoLeftShift(y, t, c)
-  y = UndoLeftShift(y, s, b)
-  return UndoRightShift(y, u)
+	y := UndoRightShift(z, l)
+	y = UndoLeftShift(y, t, c)
+	y = UndoLeftShift(y, s, b)
+	return UndoRightShift(y, u)
 }
 
 func GetRandomNumbersFromKeystream(keystream []byte) []uint32 {
-  knownNumbers := make([]uint32, len(keystream)/4)
-  for i := 0; i < len(knownNumbers); i++ {
-    n := uint32(0)
-    for j := 0; j < 4; j++ {
-      n = n + (uint32(keystream[i*4+j]) << uint32((3-j)*8))
-    }
-    knownNumbers[i] = n
-  }
-  return knownNumbers
+	knownNumbers := make([]uint32, len(keystream)/4)
+	for i := 0; i < len(knownNumbers); i++ {
+		n := uint32(0)
+		for j := 0; j < 4; j++ {
+			n = n + (uint32(keystream[i*4+j]) << uint32((3-j)*8))
+		}
+		knownNumbers[i] = n
+	}
+	return knownNumbers
 }
 
 func IsTokenMT19337Stream(token []byte) bool {
-  end := time.Now().Unix()
-  start := end - 10
-  knownNumbers := GetRandomNumbersFromKeystream(token)
-  for i := start; i <=end; i++ {
-    testMT := rng.New(uint32(i))
-    pass := true
-    for _, n := range knownNumbers {
-      if n != testMT.Next() {
-        pass = false
-        break
-      }
-    }
-    if pass {
-      return true
-    }
-  }
-  return false
+	end := time.Now().Unix()
+	start := end - 10
+	knownNumbers := GetRandomNumbersFromKeystream(token)
+	for i := start; i <= end; i++ {
+		testMT := rng.New(uint32(i))
+		pass := true
+		for _, n := range knownNumbers {
+			if n != testMT.Next() {
+				pass = false
+				break
+			}
+		}
+		if pass {
+			return true
+		}
+	}
+	return false
 }
--- set1/set1.go
+++ set1/set1.go
@@ -1,212 +1,211 @@
 package main
 
 import (
-  "bufio"
-  "encoding/hex"
-  "encoding/base64"
-  "fmt"
-  "io/ioutil"
-  "os"
+	"bufio"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"os"
 
-  "github.com/fatih/color"
-  "github.com/jgblight/matasano/pkg/utils"
-  "github.com/jgblight/matasano/pkg/ciphers"
-  "github.com/jgblight/matasano/pkg/hacks"
+	"github.com/fatih/color"
+	"github.com/jgblight/matasano/pkg/ciphers"
+	"github.com/jgblight/matasano/pkg/hacks"
+	"github.com/jgblight/matasano/pkg/utils"
 )
 
 const (
-  dataDir = "/Users/jennifer/go/src/github.com/jgblight/matasano/data/"
+	dataDir = "/Users/jennifer/go/src/github.com/jgblight/matasano/data/"
 )
 
 func problemOne(input string) (string, error) {
-    bytes, err := hex.DecodeString(input)
-    if err != nil {
-      return "", err
-    }
-    return base64.StdEncoding.EncodeToString(bytes), nil
+	bytes, err := hex.DecodeString(input)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(bytes), nil
 }
 
 func problemTwo(inputOne, inputTwo string) (string, error) {
-  bytesOne, err := hex.DecodeString(inputOne)
-  if err != nil {
-    return "", err
-  }
+	bytesOne, err := hex.DecodeString(inputOne)
+	if err != nil {
+		return "", err
+	}
 
-  bytesTwo, err := hex.DecodeString(inputTwo)
-  if err != nil {
-    return "", err
-  }
+	bytesTwo, err := hex.DecodeString(inputTwo)
+	if err != nil {
+		return "", err
+	}
 
-  return hex.EncodeToString(utils.XOR(bytesOne, bytesTwo)), nil
+	return hex.EncodeToString(utils.XOR(bytesOne, bytesTwo)), nil
 }
 
 func problemThree(input string) (string, error) {
-    bytes, err := hex.DecodeString(input)
-    if err != nil {
-      return "", err
-    }
-    result, _, _ := ciphers.DecryptSingleByteXOR(bytes)
-    return string(result), nil
+	bytes, err := hex.DecodeString(input)
+	if err != nil {
+		return "", err
+	}
+	result, _, _ := ciphers.DecryptSingleByteXOR(bytes)
+	return string(result), nil
 }
 
 func problemFour(input string) (string, error) {
-  f, err := os.Open(dataDir + input)
-  if err != nil {
-      return "", err
-  }
-  scanner := bufio.NewScanner(f)
+	f, err := os.Open(dataDir + input)
+	if err != nil {
+		return "", err
+	}
+	scanner := bufio.NewScanner(f)
 
-  bestScore := 10000.
-  var bestResult []byte
-  for scanner.Scan() {
-    str := scanner.Text()
-    bytes, err := hex.DecodeString(str)
-    if err != nil {
-      return "", err
-    }
-    result, _, score := ciphers.DecryptSingleByteXOR(bytes)
-    if score < bestScore {
-      bestScore = score
-      bestResult = result
-    }
-  }
-  return string(bestResult), nil
+	bestScore := 10000.
+	var bestResult []byte
+	for scanner.Scan() {
+		str := scanner.Text()
+		bytes, err := hex.DecodeString(str)
+		if err != nil {
+			return "", err
+		}
+		result, _, score := ciphers.DecryptSingleByteXOR(bytes)
+		if score < bestScore {
+			bestScore = score
+			bestResult = result
+		}
+	}
+	return string(bestResult), nil
 }
 
 func problemFive(input string) string {
-  bytes := []byte(input)
-  encrypted := ciphers.RepeatingKeyXOR(bytes, []byte("ICE"))
-  return hex.EncodeToString(encrypted)
+	bytes := []byte(input)
+	encrypted := ciphers.RepeatingKeyXOR(bytes, []byte("ICE"))
+	return hex.EncodeToString(encrypted)
 }
 
 func problemSix(input string) (string, string, error) {
-  text, err := ioutil.ReadFile(dataDir + input)
-  if err != nil {
-      return "", "", err
-  }
+	text, err := ioutil.ReadFile(dataDir + input)
+	if err != nil {
+		return "", "", err
+	}
 
-  bytes, err := base64.StdEncoding.DecodeString(string(text))
-  if err != nil {
-      return "", "", err
-  }
+	bytes, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return "", "", err
+	}
 
-  plaintext, key := ciphers.BreakRepeatingKeyXOR(bytes)
-  return string(plaintext), string(key), nil
+	plaintext, key := ciphers.BreakRepeatingKeyXOR(bytes)
+	return string(plaintext), string(key), nil
 }
 
 func problemSeven(input string) (string, error) {
-  text, err := ioutil.ReadFile(dataDir + input)
-  if err != nil {
-      return "", err
-  }
+	text, err := ioutil.ReadFile(dataDir + input)
+	if err != nil {
+		return "", err
+	}
 
-  bytes, err := base64.StdEncoding.DecodeString(string(text))
-  if err != nil {
-      return "", err
-  }
+	bytes, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return "", err
+	}
 
-  plaintext := ciphers.DecryptAES(bytes, []byte("YELLOW SUBMARINE"))
-  return string(plaintext), nil
+	plaintext := ciphers.DecryptAES(bytes, []byte("YELLOW SUBMARINE"))
+	return string(plaintext), nil
 }
 
 func problemEight(input string) (string, error) {
-  f, err := os.Open(dataDir + input)
-  if err != nil {
-      return "", err
-  }
-  scanner := bufio.NewScanner(f)
+	f, err := os.Open(dataDir + input)
+	if err != nil {
+		return "", err
+	}
+	scanner := bufio.NewScanner(f)
 
-  for scanner.Scan() {
-    str := scanner.Text()
-    bytes, err := hex.DecodeString(str)
-    if err != nil {
-      return "", err
-    }
+	for scanner.Scan() {
+		str := scanner.Text()
+		bytes, err := hex.DecodeString(str)
+		if err != nil {
+			return "", err
+		}
 
-    if hacks.IsECB(bytes, 16) {
-      return str, nil
-    }
+		if hacks.IsECB(bytes, 16) {
+			return str, nil
+		}
 
-  }
-  return "", nil
+	}
+	return "", nil
 }
 
 func main() {
-    header := color.New(color.FgCyan, color.Bold)
+	header := color.New(color.FgCyan, color.Bold)
 
-    header.Println("Problem 1: Convert a hex string to base 64")
-    input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    fmt.Printf("          Input: %s\n", input)
-    output, err := problemOne(input)
-    if err != nil {
-      fmt.Println(err)
-    }
-    fmt.Printf("Expected Output: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t\n")
-    fmt.Printf("  Actual Output: %s\n\n", output)
+	header.Println("Problem 1: Convert a hex string to base 64")
+	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	fmt.Printf("          Input: %s\n", input)
+	output, err := problemOne(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Expected Output: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t\n")
+	fmt.Printf("  Actual Output: %s\n\n", output)
 
+	header.Println("Problem 2: XOR two buffers")
+	inputOne := "1c0111001f010100061a024b53535009181c"
+	inputTwo := "686974207468652062756c6c277320657965"
+	fmt.Printf("          Input: %s, %s\n", inputOne, inputTwo)
+	output, err = problemTwo(inputOne, inputTwo)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Expected Output: 746865206b696420646f6e277420706c6179\n")
+	fmt.Printf("  Actual Output: %s\n\n", output)
 
-    header.Println("Problem 2: XOR two buffers")
-    inputOne := "1c0111001f010100061a024b53535009181c"
-    inputTwo := "686974207468652062756c6c277320657965"
-    fmt.Printf("          Input: %s, %s\n", inputOne, inputTwo)
-    output, err = problemTwo(inputOne, inputTwo)
-    if err != nil {
-      fmt.Println(err)
-    }
-    fmt.Printf("Expected Output: 746865206b696420646f6e277420706c6179\n")
-    fmt.Printf("  Actual Output: %s\n\n", output)
+	header.Println("Problem 3: Decrypt single-byte XOR cipher")
+	input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	fmt.Printf("          Input: %s\n", input)
+	output, err = problemThree(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("         Output: %s\n\n", output)
 
-    header.Println("Problem 3: Decrypt single-byte XOR cipher")
-    input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    fmt.Printf("          Input: %s\n", input)
-    output, err = problemThree(input)
-    if err != nil {
-      fmt.Println(err)
-    }
-    fmt.Printf("         Output: %s\n\n", output)
+	header.Println("Problem 4: Detect single-byte XOR cipher")
+	input = "set1challenge4.txt"
+	fmt.Printf("          Input: %s\n", input)
+	output, err = problemFour(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("         Output: %s\n\n", output)
 
-    header.Println("Problem 4: Detect single-byte XOR cipher")
-    input = "set1challenge4.txt"
-    fmt.Printf("          Input: %s\n", input)
-    output, err = problemFour(input)
-    if err != nil {
-      fmt.Println(err)
-    }
-    fmt.Printf("         Output: %s\n\n", output)
+	header.Println("Problem 5: Repeating Key XOR")
+	input = "Burning 'em, if you ain't quick and nimble\n" +
+		"I go crazy when I hear a cymbal"
+	fmt.Printf("          Input: %s\n", input)
+	output = problemFive(input)
+	fmt.Printf("Expected Output: 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f\n")
+	fmt.Printf("         Output: %s\n\n", output)
 
-    header.Println("Problem 5: Repeating Key XOR")
-    input = "Burning 'em, if you ain't quick and nimble\n" +
-            "I go crazy when I hear a cymbal"
-    fmt.Printf("          Input: %s\n", input)
-    output = problemFive(input)
-    fmt.Printf("Expected Output: 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f\n")
-    fmt.Printf("         Output: %s\n\n", output)
+	header.Println("Problem 6: Decrypt repeating key XOR cipher")
+	input = "set1challenge6.txt"
+	fmt.Printf("          Input: %s\n", input)
+	plaintext, key, err := problemSix(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("         Key: %s\n", key)
+	fmt.Printf("   Plaintext: %s\n\n", plaintext)
 
-    header.Println("Problem 6: Decrypt repeating key XOR cipher")
-    input = "set1challenge6.txt"
-    fmt.Printf("          Input: %s\n", input)
-    plaintext, key, err := problemSix(input)
-    if err != nil {
-      fmt.Println(err)
-    }
-    fmt.Printf("         Key: %s\n", key)
-    fmt.Printf("   Plaintext: %s\n\n", plaintext)
+	header.Println("Problem 7: Decrypt AES in ECB mode")
+	input = "set1challenge7.txt"
+	fmt.Printf("       Input: %s\n", input)
+	plaintext, err = problemSeven(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("   Plaintext: %s\n\n", plaintext)
 
-    header.Println("Problem 7: Decrypt AES in ECB mode")
-    input = "set1challenge7.txt"
-    fmt.Printf("       Input: %s\n", input)
-    plaintext, err = problemSeven(input)
-    if err != nil {
-      fmt.Println(err)
-    }
-    fmt.Printf("   Plaintext: %s\n\n", plaintext)
-
-    header.Println("Problem 8: Detect AES in ECB mode")
-    input = "set1challenge8.txt"
-    fmt.Printf("    Input: %s\n", input)
-    output, err = problemEight(input)
-    if err != nil {
-      fmt.Println(err)
-    }
-    fmt.Printf("   Output: %s\n\n", output)
+	header.Println("Problem 8: Detect AES in ECB mode")
+	input = "set1challenge8.txt"
+	fmt.Printf("    Input: %s\n", input)
+	output, err = problemEight(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("   Output: %s\n\n", output)
 }
